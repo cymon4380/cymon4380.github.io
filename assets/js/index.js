7aaf4e05d54221bb186404f4f6ae81bd7cf09f8e
cymon4380 <69615537+cymon4380@users.noreply.github.com>
2022-11-16 18:33:46 +0700

    глобальное обновление (нет)

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,7 +7,7 @@ function openSpecificLink(purpose) {
         case "tg": openUrl("https://t.me/cymon_moment_new"); break;
         case "yt": openUrl("https://youtube.com/c/CymonKSP"); break;
         case "gh": openUrl("https://github.com/cymon4380"); break;
-        case "da": openUrl("https://www.donationalerts.com/r/cymontv2004"); break;
+        case "da": openUrl("https://donationalerts.com/r/cymontv2004"); break;
     };
 }
 
@@ -19,6 +19,6 @@ $(document).ready(() => {
     var pictures = ['bg1.png', 'bg2.png', 'bg3.png', 'bg4.png'];
 
     $('.parallax-bg').hide();
-    $('.parallax-bg').css('background-image', `url(assets/img/${pictures[Math.floor(Math.random() * pictures.length)]})`);
+    $('.parallax-bg').css('background-image', `url(${pictures[Math.floor(Math.random() * pictures.length)]})`);
     $('.parallax-bg').fadeIn(500);
 });
